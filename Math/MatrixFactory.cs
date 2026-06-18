4bc0d17bf06b1437d5b06c2f5ab9114fc1d834d0
LukasMacalusso <lstrillat@gmail.com>
2026-06-18 16:37:51 -0400

    feature: Refactored Matrix4x4 to not use arrays, and use floats instead (much more efficient)

--- Math/MatrixFactory.cs
+++ Math/MatrixFactory.cs
@@ -5,101 +5,71 @@ public static class MatrixFactory
     // -- Matrix Generators -- //
     public static Matrix4x4 CreateIdentityMatrix()
     {
-
-        Matrix4x4 IdentityMatrix = new Matrix4x4();
-
-        // - Diagonal of 1's - //
-        IdentityMatrix.Elements[0, 0] = 1f;
-        IdentityMatrix.Elements[1, 1] = 1f;
-        IdentityMatrix.Elements[2, 2] = 1f;
-        IdentityMatrix.Elements[3, 3] = 1f;
-
-        return IdentityMatrix;
+        return new Matrix4x4(
+            1f, 0f, 0f, 0f,
+            0f, 1f, 0f, 0f,
+            0f, 0f, 1f, 0f,
+            0f, 0f, 0f, 1f
+        );
     }
 
     // -- Transformation Generators -- //
     public static Matrix4x4 CreateTranslationMatrix(Vector3 offset)
     {
-        Matrix4x4 translationMatrix = CreateIdentityMatrix();
-
-        // - Use the third columnn to translate by offsets - //
-        translationMatrix.Elements[0, 3] = offset.X;
-        translationMatrix.Elements[1, 3] = offset.Y;
-        translationMatrix.Elements[2, 3] = offset.Z;
-
-        return translationMatrix;
+        return new Matrix4x4(
+            1f, 0f, 0f, offset.X,
+            0f, 1f, 0f, offset.Y,
+            0f, 0f, 1f, offset.Z,
+            0f, 0f, 0f, 1f
+        );
     }
 
     public static Matrix4x4 CreateScaleMatrix(Vector3 scale)
     {
-        Matrix4x4 matrix = new Matrix4x4();
-
-        // - X, Y, & Z On the diagonal - //
-        matrix.Elements[0, 0] = scale.X;
-        matrix.Elements[1, 1] = scale.Y;
-        matrix.Elements[2, 2] = scale.Z;
-        matrix.Elements[3, 3] = 1f;
-
-        return matrix;
+        return new Matrix4x4(
+            scale.X, 0f, 0f, 0f,
+            0f, scale.Y, 0f, 0f,
+            0f, 0f, scale.Z, 0f,
+            0f, 0f, 0f, 1f
+        );
     }
 
     public static Matrix4x4 CreateRotationX(float angleRadians)
     {
-        Matrix4x4 matrix = new Matrix4x4();
         float cos = (float)System.Math.Cos(angleRadians);
         float sin = (float)System.Math.Sin(angleRadians);
 
-        // El eje X se queda igual. Se rota Y y Z.
-        matrix.Elements[0, 0] = 1f;
-
-        matrix.Elements[1, 1] = cos;
-        matrix.Elements[1, 2] = -sin;
-
-        matrix.Elements[2, 1] = sin;
-        matrix.Elements[2, 2] = cos;
-
-        matrix.Elements[3, 3] = 1f;
-
-        return matrix;
+        return new Matrix4x4(
+            1f, 0f, 0f, 0f,
+            0f, cos, -sin, 0f,
+            0f, sin, cos, 0f,
+            0f, 0f, 0f, 1f
+        );
     }
 
     public static Matrix4x4 CreateRotationY(float angleRadians)
     {
-        Matrix4x4 matrix = new Matrix4x4();
         float cos = (float)System.Math.Cos(angleRadians);
         float sin = (float)System.Math.Sin(angleRadians);
 
-        // El eje Y se queda igual. Se rota X y Z.
-        matrix.Elements[0, 0] = cos;
-        matrix.Elements[0, 2] = sin;
-
-        matrix.Elements[1, 1] = 1f;
-
-        matrix.Elements[2, 0] = -sin;
-        matrix.Elements[2, 2] = cos;
-
-        matrix.Elements[3, 3] = 1f;
-
-        return matrix;
+        return new Matrix4x4(
+            cos, 0f, sin, 0f,
+            0f, 1f, 0f, 0f,
+            -sin, 0f, cos, 0f,
+            0f, 0f, 0f, 1f
+        );
     }
 
     public static Matrix4x4 CreateRotationZ(float angleRadians)
     {
-        Matrix4x4 matrix = new Matrix4x4();
         float cos = (float)System.Math.Cos(angleRadians);
         float sin = (float)System.Math.Sin(angleRadians);
 
-        // El eje Z se queda igual. Se rota X e Y.
-        matrix.Elements[0, 0] = cos;
-        matrix.Elements[0, 1] = -sin;
-
-        matrix.Elements[1, 0] = sin;
-        matrix.Elements[1, 1] = cos;
-
-        matrix.Elements[2, 2] = 1f;
-
-        matrix.Elements[3, 3] = 1f;
-
-        return matrix;
+        return new Matrix4x4(
+            cos, -sin, 0f, 0f,
+            sin, cos, 0f, 0f,
+            0f, 0f, 1f, 0f,
+            0f, 0f, 0f, 1f
+        );
     }
 }
