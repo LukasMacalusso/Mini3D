4a595b76418ad53d3b77ddbda0ff09f29684a9ea
Ignacio Loncón <ignacioloncon2006@gmail.com>
2026-06-18 15:48:59 -0400

    feat: perspective and camera logic

--- Engine/Camera.cs
+++ Engine/Camera.cs
@@ -1,5 +1,4 @@
 namespace Mini3D.Engine;
-
 using Mini3D.Math;
 
 public class Camera
@@ -16,8 +15,37 @@ public class Camera
     // -- Matrix Generation -- //
     public Matrix4x4 GetViewMatrix()
     {
-        // TODO: Implement the "LookAt" view matrix logic
-        // This matrix shifts the entire 3D world so it is relative to the camera's perspective.
-        return new Matrix4x4();
+        // Calculate vectors
+        Matrix4x4 result = new Matrix4x4();
+
+        Vector3 forwardVector = Normalize(Position - Target);
+        
+        Vector3 tempUpVector = new Vector3(0, 1, 0);
+        Vector3 rightVector = Normalize(DotProduct(tempUpVector, forward));
+        
+        Vector3 upVector = DotProduct(forward, rightVector);
+        
+        // LookAt Matrix Construction
+        result.Elements[0, 0] = rightVector.X;
+        result.Elements[0, 1] = upVector.X;
+        result.Elements[0, 2] = forwardVector.X;
+        result.Elements[0, 3] = 0.0f;
+        
+        result.Elements[0, 0] = rightVector.Y;
+        result.Elements[0, 1] = upVector.Y;
+        result.Elements[0, 2] = forwardVector.Y;
+        result.Elements[0, 3] = 0.0f;
+        
+        result.Elements[0, 0] = rightVector.Y;
+        result.Elements[0, 1] = upVector.Y;
+        result.Elements[0, 2] = forwardVector.Y;
+        result.Elements[0, 3] = 0.0f;
+        
+        result.Elements[3, 0] = -DotProduct(rightVector, Position);
+        result.Elements[3, 1] = -DotProduct(upVector, Position);
+        result.Elements[3, 2] = -DotProduct(forwardVector, Position);
+        result.Elements[3, 3] = 1.0f;
+        
+        return result;
     }
 }
--- Engine/ProjectionMatrixFactory.cs
+++ Engine/ProjectionMatrixFactory.cs
@@ -1,5 +1,4 @@
 namespace Mini3D.Engine;
-
 using Mini3D.Math;
 
 public static class ProjectionMatrixFactory
@@ -7,9 +6,17 @@ public static class ProjectionMatrixFactory
     // -- Projection Matrices -- //
     public static Matrix4x4 CreatePerspectiveMatrix(float fovRadians, float aspect, float near, float far)
     {
-        // TODO: Implement the perspective projection matrix
-        // This matrix will ensure that objects further away appear smaller,
-        // and it prepares the 'W' component for the perspective divide.
-        return new Matrix4x4();
+        Matrix4x4 result = new Matrix4x4();
+        
+        float yScale = cot(fovRadians / 2);
+        float xScale = yScale / aspect;
+        
+        result.Elements[0, 0] = xScale;
+        result.Elements[1, 1] = yScale;
+        result.Elements[2, 2] = far / (far - near);
+        result.Elements[2, 3] = 1.0f;
+        result.Elements[3, 2] = -near * far / (far - near);
+        
+        return result;
     }
 }
